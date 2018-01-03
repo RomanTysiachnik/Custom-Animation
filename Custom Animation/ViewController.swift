4d96f02923c85bbbb757c0a4105be0b047a74342
Roman Tysiachnik <pot9ka@gmail.com>
2018-01-04 01:40:05 +0200

    Experiment #4 - Animates using Constraints

--- Custom Animation/ViewController.swift
+++ Custom Animation/ViewController.swift
@@ -10,6 +10,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var sideView: UIView!
     @IBOutlet weak var bottomView: UIView!
     @IBOutlet weak var shadowView: UIView!
+    @IBOutlet weak var bottomViewAnimationConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomViewDefaultConstraint: NSLayoutConstraint!
+    @IBOutlet weak var sideViewAnimationConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var sideViewDefaultConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,22 +28,32 @@ class ViewController: UIViewController {
     
     //Animation using UIView animate
     @IBAction func viewTapAction(_ sender: UITapGestureRecognizer) {
+        self.view.setNeedsLayout()
         if self.model.isHidden {
             UIView.animate(withDuration: 0.4, delay: 0, options: [.beginFromCurrentState, .curveEaseOut],
                 animations: {
-                    self.bottomView.frame.origin.y -= self.bottomView.frame.height
+                    self.bottomViewDefaultConstraint.isActive = true
+                    self.bottomViewAnimationConstraint.isActive = false
+                    self.sideViewDefaultConstraint.isActive = true
+                    self.sideViewAnimationConstraint.isActive = false
                     self.shadowView.alpha = 0.15
-                    self.sideView.frame.origin.x -= self.sideView.frame.width
-                    self.centerView.alpha = 1 },
+                    self.centerView.alpha = 1
+                    self.view.layoutIfNeeded()
+            },
                 completion: nil)
             self.model.changeHiddenStatus()
         } else {
             UIView.animate(withDuration: 0.4, delay: 0, options: [.beginFromCurrentState, .curveEaseOut],
                 animations: {
-                    self.bottomView.frame.origin.y += self.bottomView.frame.height
+                    self.bottomViewDefaultConstraint.isActive = false
+                    self.bottomViewAnimationConstraint.isActive = true
+                    self.sideViewDefaultConstraint.isActive = false
+                    self.sideViewAnimationConstraint.isActive = true
                     self.shadowView.alpha = 0
-                    self.sideView.frame.origin.x += self.sideView.frame.width
-                    self.centerView.alpha = 0 },
+                    self.centerView.alpha = 0
+                    self.view.layoutIfNeeded()
+                    
+            },
                 completion: nil)
             self.model.changeHiddenStatus()
         }
